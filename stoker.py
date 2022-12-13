fdbfeba752915bb927ca859a0a004d621b7ff350
Sepp Benford <14256267+sacredbeacon@users.noreply.github.com>
2022-12-13 11:56:44 -0800

    There is problem with getting ping results and calcucating the average which will led the script to crash. This version excludes that function until I fix it in the newer versions. I cleaned up the code a bit and added some comments. This version is working and tested on my machine.

--- stoker.py
+++ stoker.py
@@ -1,7 +1,7 @@
 #!/bin/bash/env python3
 
 # Stoker is a simple internet connectivity checker that logs the results to a file
-## You can find the latest release on GitHub https://github.com/sacredbeacon/stoker
+# You can find the latest release on GitHub https://github.com/sacredbeacon/stoker
 
 import os
 import time
@@ -15,11 +15,11 @@ import re
 
 # Banner
 banner = """
-  ____  _   //\  _             
- / ___|| |_|/_\|| | _____ _ __ 
- \___ \| __/ _ \| |/ / _ \ '__|
-  ___) | || (_) |   <  __/ |   
- |____/ \__\___/|_|\_\___|_|   
+ ____  _   //\  _             
+/ ___|| |_|/_\|| | _____ _ __ 
+\___ \| __/ _ \| |/ / _ \ '__|
+ ___) | || (_) |   <  __/ |   
+|____/ \__\___/|_|\_\___|_|   
 Internet Connectivity Checker
 """
 divider = "==============================="
@@ -28,7 +28,7 @@ divider = "==============================="
 print(banner)
 
 # Variables
-## Date and Time Variables
+# Date and Time Variables
 full_date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 full_date = datetime.now().strftime("%d-%m-%y")
 day_date = datetime.now().day
@@ -36,12 +36,9 @@ month_date = datetime.now().month
 year_date = datetime.now().year
 year_folder_name = str(year_date)
 month_folder_name = str(month_date)
-def sleep_time():
-    sleep_time = random.randint(10, 30)
-    return sleep_time
 
 
-## DNS Variables
+# DNS Variables
 dns_servers = ["8.8.8.8", "8.8.4.4", "1.1.1.1", "9.9.9.9", "208.67.222.222",
                "149.112.112.112", "208.67.220.220", "1.0.0.1", "8.20.247.20",
                "8.26.56.26", "185.228.169.168", "185.225.168.168", "76.76.19.19",
@@ -52,8 +49,52 @@ chosen_dns_server = random.choice(dns_servers)
 # Print divider
 print(divider)
 
-## Network Variables
-### Internet Connectivity Check
+# Main Functions
+# Defining sleep time function
+
+
+def sleep_time():
+    sleep_time = random.randint(10, 30)
+    return sleep_time
+
+# FIXME: Fix the log file directory structure based on Windows or linux
+# Creating the year folder function
+
+
+def create_year_folder():
+    if not os.path.exists(year_folder_name):
+        os.makedirs(year_folder_name)
+
+# Creating the month folder in the year folder
+
+
+def create_month_folder():
+    if not os.path.exists(year_folder_name + "/" + month_folder_name):
+        os.makedirs(year_folder_name + "/" + month_folder_name)
+
+# Creating the log file
+
+
+def create_log_file():
+    if not os.path.exists(year_folder_name + "/" + month_folder_name + "/" + full_date + ".log"):
+        log_file = open(year_folder_name + "/" +
+                        month_folder_name + "/" + full_date + ".log", "a")
+        log_file.write(
+            "Stoker Internet Connectivity Checker Log File\n"
+            "\nDate & Time: " + str(full_date_time) +
+            "\nStoker Machine: " + str(machine_name) +
+            "\nMAC Address: " + str(mac_address_str) +
+            "\nPublic IP: " + str(public_ip) +
+            "\nPrivate IP: " + str(private_ip) +
+            "\n"
+        )
+        log_file.close()
+        print("[+] Log file created")
+    else:
+        print("[-] Log file already exists")
+
+# Internet Connectivity Check``
+
 def internet_connectivity():
     while True:
         try:
@@ -65,17 +106,6 @@ def internet_connectivity():
             time.sleep(5)
             continue
 
-if internet_connectivity():
-    pass
-
-    
-### Public IP
-public_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-### Private IP
-private_ip = socket.gethostbyname(socket.gethostname())
-### MAC Address
-mac_address = uuid.getnode()
-mac_address_str = ':'.join(['{:02x}'.format((mac_address >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
 
 # Checking for the OS type
 operation_system = os.name
@@ -83,6 +113,52 @@ if operation_system == 'nt':
     operation_system = 'Windows'
 elif operation_system == 'posix':
     operation_system = 'Linux'
+else:
+    print("OS type not recognized. Exiting...")
+    exit()
+
+if operation_system == "Windows":
+    # Windows
+    ping_command = f"ping {chosen_dns_server}"
+elif operation_system == "Linux":
+    # Linux
+    ping_command = f"ping -c 4 {chosen_dns_server}"
+else:
+    print("OS type not recognized. Exiting...")
+    exit()
+
+
+# filter out the result between time= and ms and print it
+def ping_time():
+    if operation_system == "Windows":
+        ping_time = re.findall(r"time=(\d+)ms", ping_output)
+    elif operation_system == "Linux":
+        ping_time = re.findall(r"time=(\d+\.\d+) ms", ping_output)
+    return ping_time
+
+
+# Checking if the host is up or down
+def ping_results():
+    if "0%" in ping_output:
+        ping_results = "[+] Host is up"
+    elif "100%" in ping_output:
+        ping_results = "[-] Host is down"
+    else:
+        ping_results = "[!] Host seems down, trying another DNS server"
+    return ping_results
+
+# Internet Connectivity Check on start
+internet_connectivity()
+
+
+# Public IP
+public_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+# Private IP
+private_ip = socket.gethostbyname(socket.gethostname())
+# MAC Address
+mac_address = uuid.getnode()
+mac_address_str = ':'.join(['{:02x}'.format(
+    (mac_address >> ele) & 0xff) for ele in range(0, 8*6, 8)][::-1])
 
 # Machine Name
 machine_name = socket.gethostname()
@@ -99,94 +175,47 @@ print("[*] Private IP: " + private_ip)
 while True:
     # Print divider
     print(divider)
-    # Internet Connectivity Check
-    if internet_connectivity():
-        pass
     
+    # Internet Connectivity Check
+    internet_connectivity()
+
     # Printing DNS server
     print("[!] DNS server: " + chosen_dns_server)
-    
-    #FIXME: Fix the log file directory structure based on Windows or linux
+
     # Creating the year folder
-    if not os.path.exists(year_folder_name):
-        os.makedirs(year_folder_name)
-    
+    create_year_folder()
+
     # Creating the month folder in the year folder
-    if not os.path.exists(year_folder_name + "/" + month_folder_name):
-        os.makedirs(year_folder_name + "/" + month_folder_name)
-            
+    create_month_folder()
+
     # Creating the log file
-    if not os.path.exists(year_folder_name + "/" + month_folder_name + "/" + full_date + ".log"):
-        log_file = open(year_folder_name + "/" + month_folder_name + "/" + full_date + ".log", "a")
-        log_file.write(
-            "Stoker Internet Connectivity Checker Log File\n"
-            "\nDate & Time: " + str(full_date_time) +
-            "\nStoker Machine: " + str(machine_name) +
-            "\nMAC Address: " + str(mac_address_str) +
-            "\nPublic IP: " + str(public_ip) +
-            "\nPrivate IP: " + str(private_ip) +
-            "\n"
-        )
-        log_file.close()
-        print("[+] Log file created")
-    else:
-        print("[-] Log file already exists")
-        
-    # Checking the operating system type and ping the IP using the appropriate command
-    if operation_system == "Windows":
-        # Windows
-        ping_command = f"ping {chosen_dns_server}"
-    elif operation_system == "Linux":
-        # Linux
-        ping_command = f"ping -c 4 {chosen_dns_server}"
-    else:
-        print("OS type not recognized. Exiting...")
-        exit()
+    create_log_file()
+
+    # Print ping results
+    
+
     # Ping the IP address
     ping_output = os.popen(ping_command).read()
 
-    # Results 
-    if "0%" in ping_output:
-        ping_results = "[+] Host is up"
-    elif "100%" in ping_output:
-        ping_results = "[-] Host is down"
-    else:
-        ping_results = "[!] Host seems down, trying another DNS server"
-
-    # filter out the result between time= and ms and print it
-    ping_time = re.findall(r"time=(\d+)ms", ping_output)
-    # print(ping_time) average ping time
-    print("[+] Time: " + str(round(sum(map(int, ping_time)) / len(ping_time))) + "ms")
-
-    # Print the results
-    print(ping_results)
-    
     # Appending the results to the log file
-    log_file = open(year_folder_name + "/" + month_folder_name + "/" + full_date + ".log", "a")
+    log_file = open(year_folder_name + "/" +
+                    month_folder_name + "/" + full_date + ".log", "a")
     
     log_file.write("\nTime: " + datetime.now().strftime("%H:%M:%S") +
                    " DNS Server: " + chosen_dns_server +
-                   "\nResults: \n" 
-                   + ping_results +
-                   "\nTime: " + str(round(sum(map(int, ping_time)) / len(ping_time))) + "ms"
+                   "\nResults: \n" +
+                   ping_results() +
+                   "\n"
                    )
     log_file.close()
 
     # Random wait time
     print("[!] Sleeping for " + str(sleep_time()) + " seconds")
-    
+
     time.sleep(sleep_time())
-    
-    #Changing the DNS server
+
+    # Changing the DNS server
     chosen_dns_server = random.choice(dns_servers)
-    
+
     # Printing exit message
     print("[*] To exit the script, press CTRL+C")
-    
-    
-# Fixes
-#TODO: Fix the log file directorty structure
-#TODO: test on Linux
-
-# Future Features
-# add a feature to do speed test every 60 seconds and log the results
